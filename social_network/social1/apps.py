919dbd406ccf61777a0081a987ce9f7457e2be5b
Ruslan Mansurov <ruslanways@gmail.com>
2022-06-15 16:49:27 +0200

    hide SECRET_KEY to env; added app social1

---
+++ social_network/social1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Social1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'social1'
--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0kdu3is(y4#v=$*ck=xwn@-#k9t&s_j#kj(t3ys+)%@2i)u(%'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social1.apps.Social1Config'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'social_network.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
