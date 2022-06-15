d9186dc1a2115b92e85d97dcbd224c7bbe2601d6
Ruslan Mansurov <ruslanways@gmail.com>
2022-06-15 23:14:23 +0200

    added database postgres to settings.py

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social1',
+        'USER': 'socialuser',
+        'PASSWORD': os.environ['POSTGRES_SOCIALUSER_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
