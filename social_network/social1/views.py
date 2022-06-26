31aa13332daf48774d1fcd8b536bc9ea56d761e7
Ruslan Mansurov <ruslanways@gmail.com>
2022-06-26 19:24:22 +0200

    changed Welcome page text

--- social_network/social1/views.py
+++ social_network/social1/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Django.<br>Again. ))")
+    return HttpResponse("Welcome to Django.<br>Again.")
