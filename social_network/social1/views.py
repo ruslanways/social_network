4eabc8a4eef13e6e1bb5f45db6b3577a6739c2a6
Ruslan Mansurov <ruslanways@gmail.com>
2022-07-06 00:51:04 +0200

    added form to index page

--- social_network/social1/views.py
+++ social_network/social1/views.py
@@ -1,12 +1,10 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from rest_framework import generics
 from django.contrib.auth.models import User
+from .forms import LoginUserForm
 from .serializers import UserSerializer
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Welcome to Django.<br>Again.")
 
 def name(request, name):
     if name == 'Russia':
@@ -16,3 +14,15 @@ def name(request, name):
 class UsersAPIView(generics.ListAPIView):
     queryset =  User.objects.all()
     serializer_class = UserSerializer
+
+
+def index(request):
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('name', args=(request.POST['username'],)))
+    else:
+        form = LoginUserForm()
+    return render(request, 'social1/form.html', {'form': form})
+
+
